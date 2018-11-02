abfa11d6e10023b7f489f0220210729c6b082a09
beepscore <support@beepscore.com>
2018-11-02 11:53:45 -0700

    Add assertion

--- EncodedStringExpanderTests/ExpanderTests.swift
+++ EncodedStringExpanderTests/ExpanderTests.swift
@@ -21,6 +21,7 @@ class ExpanderTests: XCTestCase {
 
     func testDecoded() {
         XCTAssertEqual(Expander.decoded("2[a]"), "aa")
+        XCTAssertEqual(Expander.decoded("3[ab]"), "ababab")
     }
 
     func testMultiplierNil() {
