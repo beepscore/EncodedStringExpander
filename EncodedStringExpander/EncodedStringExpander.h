f659537efcf2df36830f510ec7ae9c35d32e6eab
beepscore <support@beepscore.com>
2018-11-02 09:29:18 -0700

    initial commit

---
+++ EncodedStringExpander/EncodedStringExpander.h
@@ -0,0 +1,19 @@
+//
+//  EncodedStringExpander.h
+//  EncodedStringExpander
+//
+//  Created by Steve Baker on 11/2/18.
+//  Copyright © 2018 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EncodedStringExpander.
+FOUNDATION_EXPORT double EncodedStringExpanderVersionNumber;
+
+//! Project version string for EncodedStringExpander.
+FOUNDATION_EXPORT const unsigned char EncodedStringExpanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EncodedStringExpander/PublicHeader.h>
+
+
